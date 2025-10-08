e3e33f6f2615bfb25167d7e05ea43dc67b9a34e4
Piumalhk <piumalharshanapro@gmail.com>
2025-10-09 00:37:08 +0530

    Add Caretaker page and set up React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,18 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Services from './components/Services'
-import FeedbackForm from './components/FeedbackForm'
-import FAQ from './components/FAQ'
-import Footer from './components/Footer'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Caretaker from './pages/caretaker.jsx';
 
 export default function App() {
   return (
-    <div style={{ padding: 24, fontFamily: 'system-ui, -apple-system, Segoe UI, Roboto, sans-serif' }}>
-      <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <Services />
-        <FeedbackForm />
-        <FAQ />
-      </main>
-      <Footer />
+    <div>
+      
+<Router>
+  <Routes>
+    <Route path="/" element={<Caretaker />} />
+    
+  </Routes>
+</Router>
+
     </div>
   )
 }
